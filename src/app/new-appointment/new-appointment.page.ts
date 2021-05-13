229745409466cf5f5756625b1d0b083fceeacc1e
Paluri <paluriss@LC1MR103KG944.pmusa.net>
2021-05-12 21:48:12 -0400

    Validation changes on View appointment and New appointment

--- src/app/new-appointment/new-appointment.page.ts
+++ src/app/new-appointment/new-appointment.page.ts
@@ -15,6 +15,15 @@ export class NewAppointmentPage implements OnInit {
   public patientInfo: any = {};
   public isPatient: boolean = false;
   public savePatientSubscription: any;
+  public Symptoms: boolean = true;
+  public Temparature: boolean = true;
+  public FeverPeaksIn: boolean = true;
+  public MucusColorTexture: boolean = true;
+  public PulseRate: boolean = true;
+  public PeculiarSymptoms: boolean = true;
+  public SleepSymptoms: boolean = true;
+  public OXIMeterReading: boolean = true;
+  public isSubmitted: boolean = false;
 
   constructor(private service: MainService, private router: Router, private aRouter: ActivatedRoute) {
     this.appointment.BodyPains = false;
@@ -45,7 +54,26 @@ export class NewAppointmentPage implements OnInit {
     return this.service.getPatientById(uid);
   }
 
+  checkValidation(){
+    console.log(this.appointment);
+    this.Symptoms = (this.appointment.Symptoms != undefined && this.appointment.Symptoms.length > 0);
+    this.Temparature = (this.appointment.Temparature != undefined && this.appointment.Temparature.length > 0)
+    this.FeverPeaksIn = (this.appointment.FeverPeaksIn != undefined && this.appointment.FeverPeaksIn.length > 0)
+    this.MucusColorTexture = (this.appointment.MucusColorTexture != undefined && this.appointment.MucusColorTexture.length > 0)
+    this.PulseRate = (this.appointment.PulseRate != undefined && this.appointment.PulseRate.length > 0)
+    this.PeculiarSymptoms = (this.appointment.PeculiarSymptoms != undefined && this.appointment.PeculiarSymptoms.length > 0)
+    this.SleepSymptoms = (this.appointment.SleepSymptoms != undefined && this.appointment.SleepSymptoms.length > 0)
+    this.OXIMeterReading = (this.appointment.OXIMeterReading != undefined && this.appointment.OXIMeterReading.length > 0)
+
+    console.log(this.appointment);
+    return (this.Symptoms && this.Temparature && this.FeverPeaksIn && this.MucusColorTexture && this.PulseRate && this.PeculiarSymptoms
+      && this.SleepSymptoms && this.OXIMeterReading);
+  }
+
   SaveAppointment(){
+    this.isSubmitted = true;
+    if(this.checkValidation())
+    {
     console.log(this.appointment);
     this.service.showLoadingSpinner();
     var userData = JSON.parse(localStorage.getItem('UserData'));
@@ -65,7 +93,12 @@ export class NewAppointmentPage implements OnInit {
     this.service.SaveAppointment(this.appointment);
     this.service.showToastMessage("Appointment Saved and sent for review !!");
     this.appointment = {};
-    this.router.navigate(['tabs']);    
+    this.router.navigate(['tabs']);
+  }
+  else
+  {
+     return false;
+  }    
   }
 
   goToHome(){
--- src/app/view-appointment/view-appointment.page.ts
+++ src/app/view-appointment/view-appointment.page.ts
@@ -16,6 +16,15 @@ export class ViewAppointmentPage implements OnInit {
   public isL2: boolean = false;
   public patientInfo: any = {};
   public apptUID: any;
+  public Symptoms: boolean = true;
+  public Temparature: boolean = true;
+  public FeverPeaksIn: boolean = true;
+  public MucusColorTexture: boolean = true;
+  public PulseRate: boolean = true;
+  public PeculiarSymptoms: boolean = true;
+  public SleepSymptoms: boolean = true;
+  public OXIMeterReading: boolean = true;
+  public isSubmitted: boolean = false;
 
   constructor(private service: MainService,
     private aservice: AuthenticationService,
@@ -64,10 +73,34 @@ export class ViewAppointmentPage implements OnInit {
     this.router.navigate(['tabs']);
   }
 
+  checkValidation(){
+    console.log(this.appointment);
+    this.Symptoms = (this.appointment.Symptoms != undefined && this.appointment.Symptoms.length > 0);
+    this.Temparature = (this.appointment.Temparature != undefined && this.appointment.Temparature.length > 0)
+    this.FeverPeaksIn = (this.appointment.FeverPeaksIn != undefined && this.appointment.FeverPeaksIn.length > 0)
+    this.MucusColorTexture = (this.appointment.MucusColorTexture != undefined && this.appointment.MucusColorTexture.length > 0)
+    this.PulseRate = (this.appointment.PulseRate != undefined && this.appointment.PulseRate.length > 0)
+    this.PeculiarSymptoms = (this.appointment.PeculiarSymptoms != undefined && this.appointment.PeculiarSymptoms.length > 0)
+    this.SleepSymptoms = (this.appointment.SleepSymptoms != undefined && this.appointment.SleepSymptoms.length > 0)
+    this.OXIMeterReading = (this.appointment.OXIMeterReading != undefined && this.appointment.OXIMeterReading.length > 0)
+
+    console.log(this.appointment);
+    return (this.Symptoms && this.Temparature && this.FeverPeaksIn && this.MucusColorTexture && this.PulseRate && this.PeculiarSymptoms
+      && this.SleepSymptoms && this.OXIMeterReading);
+  }
+
   SendToDoctor() {
+    this.isSubmitted = true;
+    if(this.checkValidation())
+    {
     this.service.showLoadingSpinner();
     this.service.sendToDoctor(this.appointment, this.aRouter.snapshot.paramMap.get('uid'));
     this.service.showToastMessage("Appointment Sent to Doctor Successfully");
+    }
+    else
+    {
+       return false;
+    }
   }
 
   NoConsultRequired(){
